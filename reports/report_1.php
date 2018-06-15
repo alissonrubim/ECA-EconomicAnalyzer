86da104c274d7f1a1a15587d8320befc4ff91648
Pedro Ferreira <Pedro Ferreira@pedro-pc>
2018-06-15 20:06:30 -0300

    Inclusão do composer e criação do PDF beneficiaries

--- reports/report_1.php
+++ reports/report_1.php
@@ -1,28 +1,66 @@
 <?php
-	require_once "../assets/php/html2pdf/html2fpdf.php";
-	$pdf = new HTML2FPDF();
-	$pdf->AddPage();
-	$pdf->WriteHTML("oi");
-	$pdf->Output();
+require_once "../DAO/conexao.php";
+require_once "../models/beneficiaries.php";
+require_once "../assets/php/vendor/autoload.php";
+require_once "../assets/php/mpdf/mpdf.php";
 
-//require_once "../models/beneficiaries.php";
-//require_once "../DAO/beneficiariesDAO.php";
-
-/*$dao = new beneficiariesDAO();
-
-$mpdf = new Mpdf();
-$html = "<div>Relatório PDF com a lista de todos os beneficiários e seus respectivos dados em ordem alfabética;</div>";
-
-$html = "<table border='1'>";
-echo sizeof($dao->getRelatorio01());
-
-foreach($dao->getRelatorio01() as $value){
-    $html = $html."<tr><td>".$value->getIdBeneficiaries()."</td><td>".$value->getStrNis()."</td><td>".$value->getStrNamePerson()."</td></tr>";
-}
-
-$html .= "</table>";
-$mpdf->WriteHTML($html);
-$mpdf->Output();*/
-
-
+ob_start();
 ?>
+<style>
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+        padding: 5px;
+    }               
+    table tr:nth-child(odd) {
+        background-color: #eee;
+    }  
+    table tr:nth-child(even) {
+        background-color: #fff;
+    }   
+    table thead th {
+        background-color: #ccc;
+    } 
+    table tfoot td {
+        background-color: #ccc;
+    } 
+</style> 
+<h1>Beneficiários</h1>
+<table class='table table-striped table-bordered'>
+    <thead>
+        <tr class='active'>
+            <th>ID</th>
+            <th>NIS</th>
+            <th>NOME</th>        
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        global $pdo;
+        try {
+        $statement = $pdo->prepare("SELECT id_beneficiaries, str_nis, str_name_person FROM tb_beneficiaries");
+        if ($statement->execute()) {
+        $rs = $statement->fetchAll(PDO::FETCH_OBJ);
+        foreach ($rs as $var) {
+        echo "<tr>
+                        <td>$var->id_beneficiaries</td>
+                        <td>$var->str_nis</td>
+                        <td>$var->str_name_person</td>        
+        </tr>";}
+
+        } else {
+        throw new PDOException("Erro: Não foi possível executar a declaração sql");
+        }
+        } catch (PDOException $erro) {
+        return "Erro: " . $erro->getMessage();
+        }
+         ?> 
+        </tbody>
+</table>
+                  
+    
+<?php
+$html = ob_get_contents();
+$mpdf = new mPDF();
+$mpdf->WriteHTML($html);
+$mpdf->Output();
